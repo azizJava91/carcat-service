29118a6d833e79108e3ab40bbccd2907d4ce7d3d
Aziz <niftalizadeaziz@gmail.com>
2026-06-23 10:02:51 +0400

    fix edit percentage error insert az ru service name,,,.

--- src/main/java/com/carland/carland_service/enums/EngineTypeTranslation.java
+++ src/main/java/com/carland/carland_service/enums/EngineTypeTranslation.java
@@ -7,25 +7,34 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum EngineTypeTranslation {
 
-    PETROL_GASOLINE("Petrol (Gasoline)", "Benzin"),
-    DIESEL("Diesel", "Dizel"),
-    HYBRID("Hybrid", "Hibrid"),
-    PLUG_IN_HYBRID("Plug-in Hybrid", "Plug-in Hibrid"),
-    ELECTRIC("Electric", "Elektro"),
-    GAS_LPG_CNG("Gas (LPG / CNG)", "Qaz"),
-    DIESEL_HYBRID("Diesel Hybrid", "Dizel-Hibrid");
+    PETROL_GASOLINE("Petrol (Gasoline)", "Benzin", "Бензин"),
+    DIESEL("Diesel", "Dizel", "Дизель"),
+    HYBRID("Hybrid", "Hibrid", "Гибрид"),
+    PLUG_IN_HYBRID("Plug-in Hybrid", "Plug-in Hibrid", "Подключаемый гибрид"),
+    ELECTRIC("Electric", "Elektro", "Электромобиль"),
+    GAS_LPG_CNG("Gas (LPG / CNG)", "Qaz", "Газ (LPG / CNG)"),
+    DIESEL_HYBRID("Diesel Hybrid", "Dizel-Hibrid", "Дизель-гибрид");
 
     private final String en;
     private final String az;
+    private final String ru;
 
     public static String translate(String enValue, String acceptLanguage) {
         for (EngineTypeTranslation type : values()) {
             if (type.en.equalsIgnoreCase(enValue)) {
-                return "az".equalsIgnoreCase(acceptLanguage)
-                        ? type.az
-                        : type.en;
+
+                if ("az".equalsIgnoreCase(acceptLanguage)) {
+                    return type.az;
+                }
+
+                if ("ru".equalsIgnoreCase(acceptLanguage)) {
+                    return type.ru;
+                }
+
+                return type.en;
             }
         }
+
         return enValue;
     }
 }
