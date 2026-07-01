32019b174ed70bdcdb4af51fbb30a98d04cc0227
Aziz <niftalizadeaziz@gmail.com>
2026-07-01 14:33:12 +0400

    added rabbit case

--- src/main/java/com/carland/carland_service/controller/WebhookController.java
+++ src/main/java/com/carland/carland_service/controller/WebhookController.java
@@ -10,6 +10,7 @@ import com.carland.carland_service.service.PartnerServiceVisitUpdateService;
 import com.carland.carland_service.util.InternalTokenValidator;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,7 +26,11 @@ import org.springframework.web.server.ResponseStatusException;
 @RestController
 @RequestMapping("/webhook/partner")
 @RequiredArgsConstructor
+@Slf4j
 public class WebhookController {
+
+    private static final String DELIVERY_SOURCE_HEADER = "X-Webhook-Delivery";
+    private static final String DELIVERY_RABBIT_REPLAY = "rabbit-replay";
 
     private final InternalTokenValidator internalTokenValidator;
     private final CarRepository carRepository;
@@ -53,15 +58,27 @@ public class WebhookController {
     }
 
     @PostMapping("/new-service-visit")
-    public ResponseEntity<PartnerNewServiceVisitResult> newServiceVisit(@RequestBody CarVinServiceHistoryV2Response request) {
+    public ResponseEntity<PartnerNewServiceVisitResult> newServiceVisit(
+            HttpServletRequest httpRequest,
+            @RequestBody CarVinServiceHistoryV2Response request) {
+        logRabbitReplayIfNeeded(httpRequest);
         PartnerNewServiceVisitResult result = partnerServiceVisitIngestService.ingest(request);
         return ResponseEntity.status(resolveIngestStatus(result)).body(result);
     }
 
     @PutMapping("/edit/service-visit")
-    public ResponseEntity<PartnerUpdateServiceVisitResult> updateServiceVisit(@RequestBody PartnerUpdateServiceVisitRequest request) {
+    public ResponseEntity<PartnerUpdateServiceVisitResult> updateServiceVisit(
+            HttpServletRequest httpRequest,
+            @RequestBody PartnerUpdateServiceVisitRequest request) {
+        logRabbitReplayIfNeeded(httpRequest);
         PartnerUpdateServiceVisitResult result = partnerServiceVisitUpdateService.update(request);
         return ResponseEntity.status(resolveUpdateStatus(result)).body(result);
+    }
+
+    private void logRabbitReplayIfNeeded(HttpServletRequest request) {
+        if (DELIVERY_RABBIT_REPLAY.equals(request.getHeader(DELIVERY_SOURCE_HEADER))) {
+            log.info("yuxudan oyandim rabbit istek gonderdi");
+        }
     }
 
     private HttpStatus resolveUpdateStatus(PartnerUpdateServiceVisitResult result) {
